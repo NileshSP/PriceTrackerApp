227f50e3518f76249073053db4debf0ada62d6df
NileshSP <emailnileshp@gmail.com>
2018-11-27 21:31:48 -0500

    updated for latest changes and revisions

--- PriceTracker/APIClient.cs
+++ PriceTracker/APIClient.cs
@@ -18,28 +18,27 @@ namespace PriceTracker
 		private const string DEFAULT_URL = "https://poloniex.com/public?command=returnTicker";
 		private const string DEFAULT_PRICEKEY = "last";
 		private const string DEFAULT_CURRPAIR = "BTC_ETH";
-		private readonly TimeSpan? DEFAULT_PRICEINTERVAL = new TimeSpan(0,0,0,60,0);// 60 seconds
+		private double DEFAULT_PRICEINTERVAL = 10;// 10 seconds
 
 		// Mutable definitons as per user's response
 		private string _url;
 		private string _priceKey;
 		private string _currPair;
 		private string _jsonPath;
-		private TimeSpan? _priceInterval;
+		private TimeSpan _priceInterval = TimeSpan.FromSeconds(60);
 
 		// Return evaluated json query path as per mutable definitions updated by user or through defaults
 		// for more details on creating json query path, please find the link: https://www.newtonsoft.com/json/help/html/SelectToken.htm 
 		private string JsonPath => string.Format("$.{0}.{1}", _currPair, _priceKey);
 
 		// Constructor initialization with finalizing variables
-		public APIClient(string url = DEFAULT_URL, string priceKey = DEFAULT_PRICEKEY, string currPair = DEFAULT_CURRPAIR, TimeSpan? priceInterval = null)
+		public APIClient(string url = DEFAULT_URL, string priceKey = DEFAULT_PRICEKEY, string currPair = DEFAULT_CURRPAIR, double? priceInterval = null)
 		{
 			this._url = url;
-			this._priceInterval = (priceInterval == null ? DEFAULT_PRICEINTERVAL : priceInterval);
-            this._priceKey = priceKey;
+			this._priceInterval = TimeSpan.FromSeconds(priceInterval ?? DEFAULT_PRICEINTERVAL);
+      this._priceKey = priceKey;
 			this._currPair = currPair;
 			this._jsonPath = JsonPath;
-
 		}
 
 		//Get sampling length for calculating simple moving average
@@ -82,78 +81,80 @@ namespace PriceTracker
 		};
 
 		// Price process starting function called from Main
-        public async void StartProcessAsync()
-        {
-			//Get user responses and set mutable variables 
-			SetUserReponse("Please enter expected currency pair to get price... (format: CURR1_CURR2(i.e. 'BTC_ETH' is by default)) and then press enter or just press enter to continue with default one",
-				(s) => { this._currPair = s; }
-			);
-			SetUserReponse("Please enter json property/column name to look for price... in API result (format: columnname(i.e. 'last' is by default)) and then press enter or just press enter to continue with default one",
-				(s) => { this._priceKey = s; }
-			);
-			SetUserReponse(string.Format("Please confirm json path as expected in api response is {0}, continue by pressing enter or provide the required one and then press enter", JsonPath),
-				(s) => { this._jsonPath = s; }
-			);
+		public void StartProcessAsync()
+		{
+			// await Task.Run(() => {
+				//Get user responses and set mutable variables 
+				SetUserReponse("Please enter expected currency pair to get price... (format: CURR1_CURR2(i.e. 'BTC_ETH' is by default)) and then press enter or just press enter to continue with default one",
+					(s) => { this._currPair = s; }
+				);
+				SetUserReponse("Please enter json property/column name to look for price... in API result (format: columnname(i.e. 'last' is by default)) and then press enter or just press enter to continue with default one",
+					(s) => { this._priceKey = s; }
+				);
+				SetUserReponse(string.Format("Please confirm json path as expected in api response is {0}, continue by pressing enter or provide the required one and then press enter", JsonPath),
+					(s) => { this._jsonPath = s; }
+				);
 
-			Console.WriteLine("Starting with interval of {0} seconds {1}", this._priceInterval.Value.TotalSeconds, Environment.NewLine);
+				Console.WriteLine("Starting with interval of {0} seconds {1}", this._priceInterval.TotalSeconds, Environment.NewLine);
 
-			int Counter = 0;
-            var lstPrices = new Dictionary<int,decimal>();
-            var aTimer = new System.Timers.Timer();
+				int Counter = 0;
+				var lstPrices = new Dictionary<int,decimal>();
+				var aTimer = new System.Timers.Timer();
 
-            aTimer.Elapsed += (s , e) =>
-            {
-				Counter++;
-                aTimer.Interval = this._priceInterval.Value.TotalMilliseconds;
-                Console.Write("{0}. Fetching price for {1} at {2} ", Counter, this._currPair, e.SignalTime);
-                var taskPrice = Task.Run(() => { 
-                    return this.GetPriceAsync((msg, ex) => 
-                        {
-                            if(ex != null) 
-                            {
-                                Console.WriteLine(" Error occured : {0}{1}Error details : {2}", msg, Environment.NewLine, ex);
-                            }
-                            else
-                            {
-                                Console.Write(msg);
-								// aTimer.Stop();
-								// throw ex;
-                            }
-                        }); 
-                    }
-                );
-                while(taskPrice.Status != TaskStatus.RanToCompletion)
-                {
-                    Console.Write(".");
-                    System.Threading.Thread.Sleep(200);
-                }
-
-                var returnPrice = taskPrice.Result;
-
-				if(!string.IsNullOrEmpty(returnPrice))
+				aTimer.Elapsed += (s , e) =>
 				{
-					Console.WriteLine(" and it is {0}", returnPrice);
-					lstPrices.Add(lstPrices.Count + 1, decimal.Parse(returnPrice));
-					decimal diffPrice = Decimal.Subtract(decimal.Parse(returnPrice), lstPrices.Where(t => t.Key == lstPrices.Count() - 2).Select(r => r.Value).FirstOrDefault());
-					
-					//Final output in console with all details
-					Console.WriteLine("Average price(out of available until now) is : {0} {1} {2} {3}"
-										, lstPrices.Select(t => t.Value).Average() 
-										, (lstPrices.Count > 1 ? 
-											string.Format("with {0} of {1} from previous price"
-												, (diffPrice == 0 ? "No Change(==)" : (diffPrice > 0 ? "Increase(+)" : "Decrease(-)"))
-												, diffPrice)
-											: "") 
-										, (SampleLength(lstPrices) > 0 ?
-											string.Format("having Simple Moving Average(SMA) of {0}", GetAverage(lstPrices))
-											: "")	
-										, Environment.NewLine
-									);
-				}
-            };
-            aTimer.Start();
-            Console.ReadLine();
-        }
+					Counter++;
+					aTimer.Interval = this._priceInterval.TotalMilliseconds;
+					Console.Write("{0}. Fetching price for {1} at {2} ", Counter, this._currPair, e.SignalTime);
+					var taskPrice = Task.Run(async() => { 
+						return await this.GetPriceAsync((msg, ex) => 
+							{
+								if(ex != null) 
+								{
+									Console.WriteLine(" Error occured : {0}{1}Error details : {2}", msg, Environment.NewLine, ex);
+								}
+								else
+								{
+									Console.Write(msg);
+									// aTimer.Stop();
+									// throw ex;
+								}
+							}); 
+						}
+					);
+					while(taskPrice.Status != TaskStatus.RanToCompletion)
+					{
+						Console.Write(".");
+						System.Threading.Thread.Sleep(200);
+					}
+
+					var returnPrice = taskPrice.Result;
+
+					if(!string.IsNullOrEmpty(returnPrice))
+					{
+						Console.WriteLine(" and it is {0}", returnPrice);
+						lstPrices.Add(lstPrices.Count + 1, decimal.Parse(returnPrice));
+						decimal diffPrice = Decimal.Subtract(decimal.Parse(returnPrice), lstPrices.Where(t => t.Key == lstPrices.Count() - 2).Select(r => r.Value).FirstOrDefault());
+						
+						//Final output in console with all details
+						Console.WriteLine("Average price(out of available until now) is : {0} {1} {2} {3}"
+											, lstPrices.Select(t => t.Value).Average() 
+											, (lstPrices.Count > 1 ? 
+												string.Format("with {0} of {1} from previous price"
+													, (diffPrice == 0 ? "No Change(==)" : (diffPrice > 0 ? "Increase(+)" : "Decrease(-)"))
+													, diffPrice)
+												: "") 
+											, (SampleLength(lstPrices) > 0 ?
+												string.Format("having Simple Moving Average(SMA) of {0}", GetAverage(lstPrices))
+												: "")	
+											, Environment.NewLine
+										);
+					}
+				};
+				aTimer.Start();
+				Console.ReadLine();
+			//});
+    }
 
 		// Function to get prices from POLONIEX API
 		// <logStatus type=Action>log or catch errors and pass back handle to caller</logStatus>
---
+++ README.md
@@ -0,0 +1,6 @@
+# PriceTracker
+Price tracking windows service app using C#.Net (.net core 2.1)
+
+
+
+![alt text](https://github.com/NileshSP/PriceTrackerApp/blob/master/screenshot.gif "Working example..")
